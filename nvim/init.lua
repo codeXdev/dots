011cc350550299a2f893f53636ff1834951bf3b5
codeXdev <anure67@gmail.com>
2024-04-18 21:47:30 +0600

    All

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,44 @@
 require("logicvolt")
+
+
+-- ~/.config/nvim/lua/options.lua
+vim.opt.number = true
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.linespace = 0
+vim.opt.visualbell = true
+vim.opt.errorbells = false
+vim.opt.ttyfast = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.clipboard = "unnamed"
+vim.opt.linespace = 10
+
+-- Set file format to Unix-style
+vim.cmd('set ff=unix')
+
+-- Enable syntax highlighting
+vim.cmd('syntax enable')
+
+-- Custom status line (similar to 'set ruler')
+vim.o.statusline = '%f %m %r'
+
+-- Enable search result highlighting
+vim.cmd('set hlsearch')
+
+-- Incremental search highlighting
+vim.cmd('set incsearch')
+
+-- Case-insensitive search
+vim.cmd('set ignorecase')
+
+-- Smart case sensitivity in search
+vim.cmd('set smartcase')
+
+
+
--- nvim/lua/logicvolt/remap.lua
+++ nvim/lua/logicvolt/remap.lua
@@ -1,3 +1,62 @@
 vim.g.mapleader = ","
+
+
+
+--------------------------------Keybindings------------------------------
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Map <leader>n to toggle_neotree function
+vim.api.nvim_set_keymap('n', '<leader>n', '<Cmd>Neotree toggle<CR><CR>', { noremap = true, silent = true })
+
+-- Navigate to previous buffer
+vim.api.nvim_set_keymap('n', '<C-;>', ':bp<CR>', { noremap = true })
+
+-- Navigate to next buffer
+vim.api.nvim_set_keymap('n', '<C-\'>', ':bn<CR>', { noremap = true })
+
+-- Switch to next tab
+vim.api.nvim_set_keymap('n', '<C-Tab>', 'gt', { noremap = true })
+
+-- Switch to previous tab
+vim.api.nvim_set_keymap('n', '<C-S-Tab>', 'gT', { noremap = true })
+
+-- Open a new tab
+vim.api.nvim_set_keymap('n', '<S-t>', ':tabnew<CR>', { noremap = true, silent = true })
+
+-- Split horizontally
+vim.api.nvim_set_keymap('n', '<Leader>h', ':<C-u>split<CR>', { noremap = true })
+
+-- Split vertically
+vim.api.nvim_set_keymap('n', '<Leader>v', ':<C-u>vsplit<CR>', { noremap = true })
+
+-- Tab Navigation
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
+
+
+--In visual mode move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+--no highlight if leader and space after search
+vim.keymap.set('n', '<Leader><space>', function()
+    vim.cmd(':nohlsearch')
+end, { expr = true, replace_keycodes = true })
+
+vim.api.nvim_set_keymap('n', '<C-enter>', ':call GuiWindowFullScreen((g:GuiWindowFullScreen + 1) % 2)<CR>', { noremap = true })
+
+---------------------------------Commands mapping--------------------------
+vim.cmd("cnoreabbrev W! w!")
+vim.cmd("cnoreabbrev Q! q!")
+vim.cmd("cnoreabbrev Qall! qall!")
+vim.cmd("cnoreabbrev Wq wq")
+vim.cmd("cnoreabbrev Wa wa")
+vim.cmd("cnoreabbrev wQ wq")
+vim.cmd("cnoreabbrev WQ wq")
+vim.cmd("cnoreabbrev W w")
+vim.cmd("cnoreabbrev Q q")
+vim.cmd("cnoreabbrev Qall qall")
